154918595d9754514f105137de45daa773285b85
sandi3ai <sandi.podrzaj@gmail.com>
2021-12-23 20:40:51 +0100

    23122021

--- src/App.js
+++ src/App.js
@@ -2,18 +2,12 @@ import React from "react";
 import Header from "./components/Header/Header";
 import Cas from "./components/Cas/Cas";
 import Login from "./components/Login/Login";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App({ Logout }) {
   return (
     <div className="App">
-      <Router>
-        <Login />
-        <Header Logout={Logout} />
-        <Cas />
-        <Route path="/logged" component={Header} />
-        <Route path="/" exact component={Login} />
-      </Router>
+      {user.email != "" ? <Header Logout={Logout} /> : <Login />}
+      <Cas />
     </div>
   );
 }
