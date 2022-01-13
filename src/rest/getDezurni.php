e83c13402af0c41dbdca7e4ef0bcc0a128652173
sandi3ai <sandi.podrzaj@gmail.com>
2022-01-13 22:15:51 +0100

    getDezurni.php working, fetching from database

---
+++ src/rest/getDezurni.php
@@ -0,0 +1,10 @@
+<?php 
+include_once('db.php');
+
+$sql = "SELECT * FROM dezurstva";
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+while($dezurstva = $stmt->fetch()) {
+    print_r($dezurstva);
+}
+?>
