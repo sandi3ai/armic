8a0e3ea9043a66251b6936a95beaa9bb717ebb0a
sandi3ai <sandi.podrzaj@gmail.com>
2022-01-13 22:39:10 +0100

    getDezurni.php working, fetching from database

--- src/rest/db.php
+++ src/rest/db.php
@@ -16,7 +16,8 @@
     // Create connection
     $conn = mysqli_connect($servername, $username, $password, $dbname);
     try {
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $options = [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'];//nastavi na utf8
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password, $options);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
