1c94e89eaa6f539bf8ce8abf43fbb7469a5a3333
sandi3ai <sandi.podrzaj@gmail.com>
2022-01-16 23:03:43 +0100

    data object created successfully in NovVnos.js, but doesnt get transfered to novDezurni.php

--- src/components/Dezurni/NovVnos.js
+++ src/components/Dezurni/NovVnos.js
@@ -3,28 +3,34 @@ import { Button, Form } from "react-bootstrap";
 import Axios from "axios";
 
 const NovVnos = () => {
-  const url = "";
+  const url = "http://localhost/reactProjects/armic/src/rest/novDezurni.php";
   const [date, setDate] = useState("");
   const [name, setName] = useState("");
 
-  function handleDateChange(event) {
-    setDate(event.target.value);
+  function submitForm(e) {
+    e.preventDefault();
+    const postData = {
+      date,
+      name,
+    };
+    console.log(postData);
+    Axios.post(url, {
+      date: date,
+      name: name,
+    }).then(() => {
+      console.log("submitForm executed");
+    });
   }
-  function handleNameChange(event) {
-    setName(event.target.value);
-  }
-  console.log(date);
-  console.log(name);
 
   return (
     <div>
       <br />
-      <Form>
+      <Form onSubmit={(e) => submitForm(e)}>
         <Form.Group className="mb-3">
           <Form.Label>Datum dežurstva: </Form.Label>
           <Form.Control
             name="date"
-            onChange={handleDateChange}
+            onChange={(e) => setDate(e.target.value)}
             value={date}
             type="date"
             placeholder="dan/mesec/leto"
@@ -39,7 +45,7 @@ const NovVnos = () => {
           <Form.Label>Izvajalec dežurstva: </Form.Label>
           <Form.Control
             name="name"
-            onChange={handleNameChange}
+            onChange={(e) => setName(e.target.value)}
             value={name}
             type="text"
             placeholder="Ime in priimek izvajalca"
