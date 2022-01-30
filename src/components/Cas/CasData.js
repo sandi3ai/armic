4e42623694dc373ed38e37a0332e0771483c22b6
sandi3ai <sandi.podrzaj@gmail.com>
2022-01-30 21:35:58 +0100

    urejena kalkulacija za število ur na posamezni datum, za urediti še noga tabele in dropdown za name namesto ID v cas.js

--- src/components/Cas/CasData.js
+++ src/components/Cas/CasData.js
@@ -1,16 +1,27 @@
 import React from "react";
 import { Table } from "react-bootstrap";
+import moment from "moment";
 
 const CasData = (data) => {
+  function subtractTime(start, finish) {
+    const zacetek = moment(start, "HH:mm:ss");
+    const konec = moment(finish, "HH:mm:ss");
+    const razlika = konec.diff(zacetek);
+    const trajanje = moment.duration(razlika);
+    const ure = trajanje.hours() + ":" + trajanje.minutes();
+    return ure;
+  }
+
   return (
     <div className="content">
-      <Table striped bordered hover>
+      <Table striped bordered hover size="sm">
         <thead>
           <tr>
             <th>Uporabnik</th>
             <th>Datum</th>
             <th>Čas začetka</th>
             <th>Čas zaključka</th>
+            <th>Število ur</th>
           </tr>
         </thead>
         <tbody>
@@ -20,9 +31,14 @@ const CasData = (data) => {
               <td>{data.datum}</td>
               <td>{data.casZacetek}</td>
               <td>{data.casKonec}</td>
+              <td>{subtractTime(data.casZacetek, data.casKonec)}</td>
             </tr>
           ))}
         </tbody>
+        <tfoot>
+          {/*Tukaj naredi skupno število ur, in mogoče število dni... mogoče
+          povprečen čas začetka, povprečen čas zaključka*/}
+        </tfoot>
       </Table>
     </div>
   );
