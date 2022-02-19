f5a7379633fa6a82fc64cbd012696f6332594865
sandi3ai <sandi.podrzaj@gmail.com>
2022-02-19 08:03:22 +0100

    Pripravljen form na modal.js, treba urediti samo še posodobitev in pošiljanje v bazo

--- src/components/Home/Modal.js
+++ src/components/Home/Modal.js
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
-import { Button } from "react-bootstrap";
+import { Button, Form, DropdownButton, Dropdown } from "react-bootstrap";
 import "./Modal.css";
 
 const Modal = ({ closeModal, passID }) => {
   const getZaposleniUrl =
     "http://localhost/reactProjects/armic/src/rest/getZaposleni.php";
+  const getUrlSkupine =
+    "http://localhost/reactProjects/armic/src/rest/getSkupine.php";
   const [data, setData] = useState([]);
+  const [updatedName, setUpdatedName] = useState("");
+  const [skupine, setSkupine] = useState([{ id: "", name: "" }]);
+  const [updatedSkupina, setUpdatedSkupina] = useState("");
 
   const getZaposleni = () => {
     try {
@@ -30,6 +35,16 @@ const Modal = ({ closeModal, passID }) => {
     getZaposleni();
   }, []);
 
+  const getSkupine = () => {
+    try {
+      Axios.get(getUrlSkupine).then((response) => {
+        setSkupine(response.data.skupine);
+      });
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
   return (
     <div className="modalBackground">
       <div className="modalContainer">
@@ -39,9 +54,43 @@ const Modal = ({ closeModal, passID }) => {
         <div className="title">
           <h4>{getZaposleniIme(data, passID)} - posodobi podatke</h4>
         </div>
-        <div className="body">Ime: Form:</div>
-        <div className="footer">
-          <Button variant="outline-success">Posodobi</Button>
+        <div className="body">
+          <Form>
+            <Form.Group className="mb-3">
+              <Form.Label>Spremeni ime in priimek zaposlenega: </Form.Label>
+              <Form.Control
+                name="name"
+                onChange={(e) => setUpdatedName(e.target.value)}
+                defaultValue={getZaposleniIme(data, passID)}
+                type="text"
+              />
+            </Form.Group>
+            {updatedName}
+
+            <Form.Label>Delavcu spremeni skupino: </Form.Label>
+            <DropdownButton
+              variant="outline-primary"
+              title={updatedSkupina}
+              onClick={(e) => getSkupine(e)}
+              onSelect={(e) => setUpdatedSkupina(e)}
+              value={updatedSkupina}
+              drop="down"
+            >
+              {skupine.map((skupina, i) => (
+                <Dropdown.Item key={i} eventKey={skupina.skupinaID}>
+                  {skupina.skupinaIme}
+                </Dropdown.Item>
+              ))}
+            </DropdownButton>
+            <br />
+            <br />
+            <div className="successBox">
+              <Button variant="outline-success" type="submit">
+                Posodobi
+              </Button>
+              {/*successTxt && " Nov zaposleni uspešno dodan!"*/}
+            </div>
+          </Form>
         </div>
       </div>
     </div>
