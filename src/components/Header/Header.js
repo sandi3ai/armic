e03d2773bb25d6ecc6e12db878972c824c08cdfa
sandi3ai <sandi.podrzaj@gmailc.om>
2024-03-15 01:54:58 +0100

    Fixed badges on prosnje not showing on app render

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavigationBar from "./NavBar.js";
+import { post } from "../../Helper";
+import { useCounts } from "../../hooks/CountsContext";
 
 function Header({ Logout }) {
-  //const { data, loading } = useApi("https://localhost/armic_api/time.php");
+  const countProsnjeUrl = `${process.env.REACT_APP_BASE_URL}/src/rest/countProsnje.php`;
+  const { updateCounts } = useCounts();
+
+  const countProsnje = async () => {
+    try {
+      post(countProsnjeUrl, {}).then((response) => {
+        console.log("Count prosnje response: ", response);
+        updateCounts({
+          countNadure: response.data.countNadure,
+          countOdsotnost: response.data.countOdsotnost,
+        });
+      });
+    } catch (error) {
+      console.error("Failed to fetch counts", error);
+    }
+  };
+
+  useEffect(() => {
+    countProsnje();
+  }, []);
 
   return (
     <div>
